f4bf6edca61c07197c8f8b56be362159ef1013b6
shubhanshu <shubhanshupandey7654321@gmail.com>
2025-04-01 15:32:04 +0530

    feat: Initial setup of Spring Boot backend and Angular frontend projects

---
+++ backend/resume-api/src/test/java/com/example/resume_api/ResumeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
